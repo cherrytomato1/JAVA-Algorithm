def5759bc96fc380c65210325b53c479aa415ff1
cherrytomato1 <terec@naver.com>
2021-01-16 20:00:35 +0900

    [comment]1110_더하기 사이클

--- src/a0001_210116_1110_b1/Main.java
+++ src/a0001_210116_1110_b1/Main.java
@@ -20,19 +20,19 @@ public class Main
         Scanner sc = new Scanner(System.in);
         String str = sc.nextLine();
 
-        int N = Integer.parseInt(str);
-        int ans = N ;
-        int f , b ;
-        int cnt = 0 ;
+        int N = Integer.parseInt(str);          //스캐너로 입력받은 값을 Int 형으로 변환
+        int ans = N ;                           //결과 변수 선언, 초기값은 입력값과 같이하여 연산 수행
+        int f , b ;                             //두 자리수의 십의 자리와 일의 자리를 저장하는 변수 선언
+        int cnt = 0 ;                           //총 연산의 회수
 
         do
         {
-            b = ans % 10 ;
-            f = ans / 10 ;
-            ans = (b * 10) + ((f + b) % 10);
+            b = ans % 10 ;                      //일의 자리는 10으로 나눈 값의 나머지
+            f = ans / 10 ;                      //십의 자리는 10으로 나눈 값의 몫
+            ans = (b * 10) + ((f + b) % 10);    //그 다음 값은 일의 자리에 10을 곱해 십의 자리로 보내고, 앞선 결과값의 합에서 일의 자리를 더한다.
 
-            cnt ++;
-        } while ( ans != N ) ;
+            cnt ++;                             //총 연산회수 증가
+        } while ( ans != N ) ;                  //입력값과 같으면 연산 종
 
         System.out.println(cnt);
     }
