7d0c9fb3d6a7fac2693491d9f443a3de5c1345ac
cherrytomato1 <terec@naver.com>
2021-01-18 20:05:11 +0900

    [test] First controller, logger

---
+++ src/testPackage/Main.java
@@ -0,0 +1,14 @@
+package testPackage;
+
+public class Main {
+    public static void main(String[] args) {
+        String str1 = "test";
+        String str2 = new String("test2");
+        String str3 = str1 + str2;
+        int N = 2532;
+
+
+        System.out.println(str1 + str2 + N);
+        System.out.println(str3);
+    }
+}
