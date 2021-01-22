f2bca75209fa18ea2ce0614df0684a480a11e27d
cherrytomato1 <terec@naver.com>
2021-01-22 22:14:36 +0900

    [problem]0004_2447

---
+++ src/a0004_210121_2447_s1/Main.java
@@ -0,0 +1,13 @@
+package a0004_210121_2447_s1;
+
+public class Main {
+    static boolean[] map;
+    static int row, col;
+    static void f(int n){
+
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
