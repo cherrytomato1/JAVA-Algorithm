ae2b7867a5f2b13f351057107f9376c3c3f88ebc
cherrytomato1 <terec@naver.com>
2021-02-01 23:28:05 +0900

    solve : 백준 1244 스위치 끄고 켜기

---
+++ src/boj0017_210201_1244_s4/Main.java
@@ -0,0 +1,46 @@
+package boj0017_210201_1244_s4;
+
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class Main {
+    static int N;
+    static int M;
+    static boolean[] arr;
+    static StringTokenizer st;
+
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+    static void solve() throws IOException{
+        M = Integer.parseInt(br.readLine());
+
+        for( int i = 0 ; i < M ; i++){
+            st = new StringTokenizer(br.readLine());
+            int gen = Integer.parseInt(st.nextToken());
+            int num = Integer.parseInt(st.nextToken());
+
+            for(int j = num ; gen == 1 && j <= N ; j += num){
+                arr[j] = !arr[j];
+            }
+            for(int j = num, k = num ; gen == 2 && j <= N && k > 0 ; j++, k--){
+                if(arr[j] == arr[k]) arr[j] = arr[k] = !arr[j];
+                else break;
+            }
+        }
+        for(int i = 1 ; i <= N; i++){
+            System.out.print((arr[i] ? 1 : 0) + " ");
+            if(i % 20 == 0) System.out.println();
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        N = Integer.parseInt(br.readLine());
+        arr = new boolean[N + 1];
+        st = new StringTokenizer(br.readLine());
+
+        for(int i = 1 ; i <= N ; i++){
+            arr[i] = st.nextToken().equals("1");
+        }
+        solve();
+    }
+}
