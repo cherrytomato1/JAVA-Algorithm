ae40c42d641567e1d0fcfaeab05642bf1d5c21ab
cherrytomato1 <terec@naver.com>
2021-02-28 17:35:50 +0900

    solve : 백준 1662번 압축

--- src/boj/boj0018_210201_1662_g5/백준_1662_압축.java
+++ src/boj/boj0018_210201_1662_g5/백준_1662_압축.java
@@ -1,29 +1,36 @@
 package boj.boj0018_210201_1662_g5;
 
 import java.io.*;
-import java.util.ArrayDeque;
-import java.util.Deque;
 
 public class 백준_1662_압축 {
 	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-	static char[] s;
-	static Deque<Character> stk = new ArrayDeque<>();
+	static char[] arr;
 
-	static void init() throws IOException{
-		s = br.readLine().toCharArray();
+	static int[] f(int idx, int mod){
+		int val = 0;
+		int skipIdx = -1;
+		for(int i = idx ; i < arr.length; i++){
+			if(i <= skipIdx)  continue;
+
+			if(arr[i] == '('){
+				int[] returns = f(i + 1, arr[i - 1] - '0');
+				val += returns[0] - 1;
+				skipIdx = returns[1];
+			}else if(arr[i] == ')'){
+				return new int[]{val * mod, i};
+			}
+			else val++;
+		}
+		return new int[]{val * mod, 0};
 	}
 
-
+	static void init() throws IOException{
+		arr = br.readLine().toCharArray();
+	}
 
 	public static void main(String[] args) throws IOException{
 		init();
-
-		for(int i = 0 ; i < s.length; i++){
-
-		}
-		//System.out.println(dfs(0, 0));
+		System.out.println(f(0,1)[0]);
 	}
 }
-
-
