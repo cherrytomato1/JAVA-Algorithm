8da06befb85b8d353376ed64e3786ee06c61ccec
cherrytomato1 <terec@naver.com>
2021-03-17 01:28:36 +0900

    algorithm : 인접 리스트 꼬리에 삽입 - BFS

---
+++ src/boj/boj0067_210317_1260_s2/백준_1260_DFS와BFS.java
@@ -0,0 +1,90 @@
+package boj.boj0067_210317_1260_s2;
+
+import java.util.*;
+import java.io.*;
+
+public class 백준_1260_DFS와BFS {
+
+	private static class Node{
+		int vertex;
+		Node next;
+
+		public Node(){}
+
+		public Node(int vertex){
+			this.vertex = vertex;
+		}
+	}
+
+	private static class NodeHead extends Node{
+		Node tail;
+		public NodeHead(){
+			super();
+		}
+		public void addTail(Node n){
+			if(tail == null){
+				this.next = n;
+				this.tail = n;
+				System.out.println("new tail " + n.vertex);
+				return;
+			}
+			System.out.println("tail add " + n.vertex);
+			tail.next = n;
+			tail = n;
+		}
+	}
+	private static final StringBuilder sb = new StringBuilder();
+
+	private static NodeHead[] graph;
+	private static int N;
+	private static int M;
+
+	private static int init() throws IOException{
+		final BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st;
+
+		st = new StringTokenizer(br.readLine());
+		N = Integer.parseInt(st.nextToken());
+		M = Integer.parseInt(st.nextToken());
+		int start = Integer.parseInt(st.nextToken());
+
+		graph = new NodeHead[N + 1];
+		for(int i = 1 ; i <= N; i++){
+			graph[i] = new NodeHead();
+		}
+
+		for(int i = 0 ; i < M ; i++){
+			st = new StringTokenizer(br.readLine());
+			int from = Integer.parseInt(st.nextToken());
+			int to = Integer.parseInt(st.nextToken());
+			graph[from].addTail(new Node(to));
+			graph[to].addTail(new Node(from));
+		}
+		return start;
+	}
+
+	private static void bfs(int start){
+		Deque<Integer> q = new ArrayDeque<>();
+		int visited = 1 << start;
+		q.offer(start);
+		while(!q.isEmpty()){
+			int curr = q.poll();
+			sb.append(curr).append(" ");
+			System.out.println(graph[curr].next.vertex);
+			for(Node temp = graph[curr].next; temp != null; temp = temp.next){
+
+				int vertex = temp.vertex;
+				//System.out.println(vertex);
+				if((visited & 1 << vertex) != 0)   continue;
+				visited |= 1 << vertex;
+
+				q.offer(vertex);
+			}
+		}
+	}
+	public static void main(String[] args) throws IOException{
+		int start = init();
+		bfs(start);
+		System.out.println(sb.toString());
+	}
+}
