e667a9649c7a975407c1d8c7059c6fab71ca133b
cherrytomato1 <terec@naver.com>
2021-03-24 15:21:11 +0900

    solve : 정올 1077번 배낭채우기1

---
+++ src/jungol/jungol_1077_배낭채우기1/정올_1077_배낭채우기1.java
@@ -0,0 +1,38 @@
+package jungol.jungol_1077_배낭채우기1;
+
+import java.util.*;
+import java.io.*;
+
+public class 정올_1077_배낭채우기1 {
+	private static final BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	private static StringTokenizer st;
+
+	private static int N;
+	private static int W;
+	private static int[] d;
+
+	private static void init() throws IOException{
+		st = new StringTokenizer(br.readLine().trim(), " ");
+		N = Integer.parseInt(st.nextToken());
+		W = Integer.parseInt(st.nextToken());
+
+		d = new int[W + 1];
+	}
+
+	private static int solve() throws IOException{
+		for(int i = 0 ; i < N; i++){
+			st = new StringTokenizer(br.readLine().trim());
+			int weight = Integer.parseInt(st.nextToken());
+			int value = Integer.parseInt(st.nextToken());
+			for(int j = 1; j <= W ; j++){
+				if(j >= weight)     d[j] = Math.max(d[j - weight] + value, d[j]);
+			}
+		}
+		return d[W];
+	}
+
+	public static void main(String[] args) throws IOException{
+		init();
+		System.out.println(solve());
+	}
+}
